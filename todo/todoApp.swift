b2347c47cf0cf27a67efc7bf2152abdd4e65e673
Name <ahmad.awaab@gmail.com>
2023-04-08 15:44:09 -0400

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Ahmad Hossain on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
